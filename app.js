d0daa751467451b35880d4bea3c7e106abfa2754
Paul McGeachie <paulmcgeachie@icloud.com>
2022-02-02 07:24:37 +0000

    add inline style using v-bind

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      boxAselected: false,
-      boxBselected: false,
-      boxCselected: false,
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
 });
